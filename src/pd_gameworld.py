fc1b17c9cd10835dd7e4c27bcb48537a9d479cd2
gemida <danielgeminger@gmail.com>
2020-08-09 17:01:48 +0200

    Discord integration

--- src/pd_gameworld.py
+++ src/pd_gameworld.py
@@ -3,4 +3,4 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="!pd")
 
 
-bot.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.3SotSQYvXm7DaQO_zCx89z8ILpQ")
+bot.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.M_1EwGyle3wi9d3yc4JzFqcENcY")
