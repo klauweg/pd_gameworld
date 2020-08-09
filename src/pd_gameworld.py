d65e9687c2ff36e9171efc8c57bd875099d0ff9f
PlaceblockHD <felix.weglehner@gmx.de>
2020-08-09 18:03:29 +0200

    Added Join Messages

--- src/pd_gameworld.py
+++ src/pd_gameworld.py
@@ -1,6 +1,12 @@
-from discord.ext import commands
+import discord
 
-bot = commands.Bot(command_prefix="!pd")
+client = discord.Client()
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(741965363549569034)
+    await channel.send(f"""Welcome to the Server {member.mention} !""")
+
+client.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.M_1EwGyle3wi9d3yc4JzFqcENcY")
 
 
-bot.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.M_1EwGyle3wi9d3yc4JzFqcENcY")
