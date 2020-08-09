b046477845f212314d0a8d1a4ffbb11610ed934b
gemida <danielgeminger@gmail.com>
2020-08-09 20:23:06 +0200

    Added auto role for level 1

--- src/pd_gameworld.py
+++ src/pd_gameworld.py
@@ -1,12 +1,13 @@
+from discord.ext import commands
 import discord
+bot = commands.Bot(command_prefix="!pd")
 
-client = discord.Client()
 
-@client.event
-async def on_member_join(member):
-    channel = client.get_channel(741965363549569034)
+@bot.event
+async def on_member_join(member: discord.Member):
+    channel = bot.get_channel(741965363549569034)
+    role = member.guild.get_role(741836468674101270)
+    await member.add_roles(role)
     await channel.send(f"""Welcome to the Server {member.mention} !""")
 
-client.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.M_1EwGyle3wi9d3yc4JzFqcENcY")
-
-
+bot.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.M_1EwGyle3wi9d3yc4JzFqcENcY")
