281898f17fce2a2cef689e03b59ac9a053755e62
PlaceblockHD <felix.weglehner@gmx.de>
2020-08-10 22:56:39 +0200

    Connect four with discord integration but only queue join without game controll

--- src/pd_gameworld.py
+++ src/pd_gameworld.py
@@ -1,14 +1,11 @@
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!pd")
+from src.connectfour.Gamelogic import Gamelogic
 
+bot = commands.Bot(command_prefix="!")
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(741965363549569034)
-    await channel.send(f"""Welcome to the Server {member.mention} !""")
-    role = member.guild.get_role(741836468674101270)
-    member.add_roles(role)
+connectfour = Gamelogic(bot)
+bot.add_cog(connectfour)
 
 bot.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.M_1EwGyle3wi9d3yc4JzFqcENcY")
 
