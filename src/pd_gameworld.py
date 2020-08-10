0d59aee16a5fbcc21b0696271acb9fefdb0816de
PlaceblockHD <felix.weglehner@gmx.de>
2020-08-10 18:01:54 +0200

    Connectfour finsihed but without discord integration

--- src/pd_gameworld.py
+++ src/pd_gameworld.py
@@ -1,20 +1,14 @@
 from discord.ext import commands
-from discord.utils import get
 
-bot = commands.Bot(command_prefix='!pd')
-
-
-channel = bot.get_channel(741965363549569034)
+bot = commands.Bot(command_prefix="!pd")
 
 
 @bot.event
 async def on_member_join(member):
+    channel = bot.get_channel(741965363549569034)
     await channel.send(f"""Welcome to the Server {member.mention} !""")
-    role = get(member.server.roles, id="741836468674101270")
-    await bot.add_roles(member, role)
-
-
-
+    role = member.guild.get_role(741836468674101270)
+    member.add_roles(role)
 
 bot.run("NzQyMDMyMDAzMTI1MzQ2MzQ0.XzANJw.M_1EwGyle3wi9d3yc4JzFqcENcY")
 
