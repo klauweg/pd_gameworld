50a4fa993cddf597172113eeae14668cd5e7b243
PlaceblockHD <53298953+PlaceblockHD@users.noreply.github.com>
2020-08-11 09:11:47 +0200

    Update pd_gameworld.py
    Changed command prefix to "!" now the commands are: !tictactoe , !connectfour
    Added on_command_error

--- src/pd_gameworld.py
+++ src/pd_gameworld.py
@@ -1,13 +1,20 @@
 from discord.ext import commands
 from connectfour.Gamelogic import ConnectFourGameLogic
 from tictactoe.GameLogic import TicTacToeGameLogic
-client = commands.Bot(command_prefix="!pd")
+from discord.ext.commands import CommandNotFound
+client = commands.Bot(command_prefix="!")
 
 
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(741965363549569034)
     await channel.send(f"""Welcome to the Server {member.mention} !""")
+    
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return
+    raise error
 
 client.add_cog(ConnectFourGameLogic(client))
 client.add_cog(TicTacToeGameLogic(client))
