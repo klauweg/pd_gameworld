473256f8293139b4724013f94918e43291cf6a0a
PlaceblockHD <felix.weglehner@gmx.de>
2020-08-12 15:30:51 +0200

    1. Changed the Prefix to "!" again
    2. Wrong command listener is missing on gamelogic (if you enter wrong command there is an error)
    
    3. Added Discord integration for ConnectFour (without testing for finsih or anything)

--- src/connectfour/Game.py
+++ src/connectfour/Game.py
@@ -1,19 +1,20 @@
+import discord
 import numpy as np
 from discord.ext import commands
 
-class Game(commands.Cog):
 
-    def __init__(self, playerlistids, cid, botvar):
+class ConnectFourGame(commands.Cog):
+
+    def __init__(self, playerlistids, cid, botvar, message, gamefield):
         self.row_count = 6
         self.column_count = 7
         self.turn = 2
         self.playerids = playerlistids
         self.channelid = cid
         self.bot = botvar
-        self.gamefield = np.zeros((self.row_count, self.column_count))
-        self.print_gamefield()
-        self.gamefield_message = None
+        self.gamefield = gamefield
         self.aktplayer = 0
+        self.gamefield_message: discord.Message = message
 
     async def insert_selected(self, row, col, playerindex):
         self.gamefield[row][col] = playerindex + 1
@@ -23,25 +24,18 @@ class Game(commands.Cog):
 
     async def get_next_row(self,col):
         for r in range(self.row_count):
-            if (self.gamefield[r][col] == 0):
+            if self.gamefield[r][col] == 0:
                 return r
 
     async def print_gamefield(self):
-        if(self.gamefield_message == None):
-            message = self.bot.get_channel(self.channelid).send(str(self.gamefield))
-            await message.add_reaction('\u2776')
-            await message.add_reaction('\u2777')
-            await message.add_reaction('\u2778')
-            await message.add_reaction('\u2779')
-            await message.add_reaction('\u277A')
-            await message.add_reaction('\u277B')
-            await message.add_reaction('\u277C')
-            self.gamefield_message = message
-        else:
-            self.gamefield_message.edit(content=str(self.gamefield))
+        embed = discord.Embed(title="", description=str(np.flip(self.gamefield, 0)), color=discord.Color.green())
+        embed.set_author(name="ConnectFour",
+                         icon_url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
+        await self.gamefield_message.edit(embed = embed)
 
-
-    async def check_state(self,piece):
+    async def check_state(self, piece):
         piece += 1
         for c in range(self.column_count):
             for r in range(self.row_count):
@@ -60,32 +54,33 @@ class Game(commands.Cog):
         return True
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, payload):
-        if payload.channel_id == self.channelid:
-            if payload.user_id == self.playerids[self.aktplayer]:
+    async def on_reaction_add(self, payload: discord.Reaction, user):
+        if payload.message.id == self.gamefield_message.id:
+            if user.id == self.playerids[self.aktplayer]:
 
                 emojis = {
-                    "68546f5fc3b2166f42cf90b7e23c5ae9" : 0,
-                    "eb29ce5fcf54bc3b23ff77039a4ecf3c" : 1,
-                    "67f896405747f26f63f09e0cb048d358" : 2,
-                    "09fe8a2882cac4cdb4712ab9622d3fe1" : 3,
-                    "5575865e2cb3d50ea051b09d7e1d2550" : 4,
-                    "f8b3e0e54d99d2b2962a2e474b2110e4" : 5,
-                    "c5ef2ff553f9cecd81add57e79aaf81d" : 6
+                    "1️⃣": 0,
+                    '2️⃣': 1,
+                    '3️⃣': 2,
+                    '4️⃣': 3,
+                    '5️⃣': 4,
+                    '6️⃣': 5,
+                    "7️⃣": 6
                 }
 
-                col = emojis[payload.emoji.id]
+                col = emojis[payload.emoji]
                 if(col != None):
-                    if (self.is_location_valid(self.gamefield, col)):
-                        row = self.get_next_row(self.gamefield, col)
-                        self.insert_selected(self.gamefield, row, col, self.aktplayer)
-                    if self.check_state(self.gamefield, self.aktplayer) == False:
+                    if await self.is_location_valid(col):
+                        row = await self.get_next_row(col)
+                        await self.insert_selected(row, col, self.aktplayer)
+                        await self.print_gamefield()
+                    if not await self.check_state(self.aktplayer):
                         # TODO: SEND MESSAGE TO PLAYERS THAT A PLAYER WON AND KICK THEM OUT OF THE GAME
                         # TODO: DELETE GAME MESSAGE TO PREVENT SO MANY MESSAGES
                         print(self.bot.get_user(self.playerids[self.aktplayer]).display_name + " won the Game!")
                         return
 
-                await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id).remove_reaction(payload.emoji, self.bot.get_user(payload.user_id))
+                await self.gamefield_message.remove_reaction(payload.emoji, self.bot.get_user(user.id))
                 self.aktplayer += 1
                 self.aktplayer % self.aktplayer / 2
 
--- src/connectfour/Gamelogic.py
+++ src/connectfour/Gamelogic.py
@@ -1,4 +1,6 @@
-from src.connectfour import Game
+import numpy as np
+
+from connectfour.Game import ConnectFourGame
 from discord.ext import commands
 import discord
 
@@ -12,7 +14,7 @@ class ConnectFourGameLogic(commands.Cog):
             742407072125157377
         ]
         self.queue = []
-        self.bot = bot
+        self.bot: commands.Bot = bot
         self.joinchannel = 742407492520378418
 
     #Is a Channel id availible to play?
@@ -34,15 +36,27 @@ class ConnectFourGameLogic(commands.Cog):
         while(len(self.queue) > 1):
             channelid = await self.get_availible_channel_id()
             if not channelid == False:
-                gameplayerids = [self.queue.pop(0), self.queue.pop(1)]
-                gameobject = Game(gameplayerids, channelid, self.bot)
-                self.bot.add_cog(gameobject)
-                self.games.append(gameobject)
+                gameplayerids = [self.queue.pop(0),self.queue.pop(0)]
+                gamefield = np.zeros((6, 7))
                 embed = discord.Embed(title="You can play now!", description="Your are Playing in Channel **" + self.bot.get_channel(channelid).name + "** !", color=0x2dff32)
                 embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
                 embed.set_footer(text="Thanks for Playing!")
                 channel = self.bot.get_channel(self.joinchannel)
                 await channel.send(embed=embed)
+                embed = discord.Embed(title="",description=str(np.flip(gamefield)),color=discord.Color.green())
+                embed.set_author(name="ConnectFour",icon_url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
+                message = await self.bot.get_channel(channelid).send(embed=embed)
+                gameobject = ConnectFourGame(gameplayerids, channelid, self.bot, message, gamefield)
+                self.bot.add_cog(gameobject)
+                self.games.append(gameobject)
+                await message.add_reaction("1️⃣")
+                await message.add_reaction('2️⃣')
+                await message.add_reaction('3️⃣')
+                await message.add_reaction('4️⃣')
+                await message.add_reaction('5️⃣')
+                await message.add_reaction('6️⃣')
+                await message.add_reaction("7️⃣")
                 break
             else:
                 embed = discord.Embed(title="Oh no!",description="There are enough players in the queue but at the moment there is no free channel to play! Sry!",color=0xff812d)
@@ -53,14 +67,9 @@ class ConnectFourGameLogic(commands.Cog):
                 channel = self.bot.get_channel(self.joinchannel)
                 await channel.send(embed=embed)
                 return
-        embed = discord.Embed(title="Oh No!",description=" Sorry but at the moment only you play! Please be patient and wait for a second player",color=0xff6d0d)
-        embed.set_author(name="ConnectFour", icon_url = "https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
-        channel = self.bot.get_channel(self.joinchannel)
-        await channel.send(embed=embed)
 
     @commands.command()
-    async def connectfour(self, ctx: discord.ext.commands.Context, *, member: discord.Member = None):
+    async def connectfour(self, ctx: commands.Context, *, member: discord.Member = None):
         member = ctx.author or member
 
         commandchannel = ctx.channel
@@ -72,17 +81,13 @@ class ConnectFourGameLogic(commands.Cog):
                 embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
                 await ctx.channel.send(embed=embed)
                 return
-            await self.add_to_queue(member.id)
             embed = discord.Embed(title="Nice!", description=f"""{member.display_name} Joined the Queue""", color=0x49ff35)
             embed.set_author(name="ConnectFour", icon_url = "https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
             embed.add_field(name="But:", value="It may take a moment for the game to start, so sit back and relax", inline=False)
             embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
-            queueplayernames = ""
-            for memberid in self.queue:
-                queueplayernames = queueplayernames + (member.display_name + " ")
-            embed.add_field(name="Queue:", value=queueplayernames, inline=False)
             embed.set_footer(text="Thanks vor Playing!")
             await ctx.channel.send(embed=embed)
+            await self.add_to_queue(member.id)
 
 
 
