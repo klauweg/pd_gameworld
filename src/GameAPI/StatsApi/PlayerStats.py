9040fc62eb9c4b99446604e1dff549a42993381e
Klaus W <vortex25@gmx.de>
2020-08-15 01:48:53 +0200

    Wir machen mal einen contextmanager für die JSON Modifikationen.

--- src/GameAPI/StatsApi/PlayerStats.py
+++ src/GameAPI/StatsApi/PlayerStats.py
@@ -1,45 +1,49 @@
 import json
 
+filename="player_data_new.json"
 
-filename="resources/player_data_new.json"
+# Wir erzeugen einen Context Manager, welcher die JSON Daten lädt, ggf.
+# die Datei erzeugt, und ggf. den Benutzer anlegt und weiterhin das Aktualisieren
+# der Daten im JSON File sicher stellt.
+class ModStatsData():
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-def load_data():
-    try:
-        file = open( filename, "r")
-        data = json.load(file)
-        file.close()
-    except:
-        print("File existiert nicht, leeres Dictionary erzeugen...")
-        data = {}
-    return data
+    def __enter__(self):  # vor der Modifikation
+        try:
+            file = open( filename, "r")
+            self.data = json.load(file)
+            file.close()
+        except:
+            print("File existiert nicht, leeres Dictionary erzeugen...")
+            self.data = {}
+        
+        if self.user_id in self.data: # Existiert der Benutzer?
+            self.user = self.data[self.user_id] # Daten des Benutzer extrahieren
+        else:
+            self.user = { "xp": 0 } # sonst neuen Benutzer mit xp=0 anlegen
+        return self
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback): # nach der Modifikation
+        self.data[ self.user_id ] = self.user
+        with open(filename, "w") as file:
+            json.dump( self.data, file, indent=4)
 
-def modify_user( user_id, todo ):
-    data = load_data()
+#############################################################################
+# API Functions:
+# (Nicht existierende Benutzer werden vom Context-Manager angelegt.)
 
-    if user_id in data: # Existiert der Benutzer?
-        userdata = data[user_id] # Daten des Benutzer extrahieren
-    else:
-        userdata = { "xp": 0 } # sonst neuen Benutzer mit xp=0 anlegen
-
-    userdata = todo( userdata ) # Vordefinierte to do Funktion ausführen
-    data[user_id]=userdata   # Userdaten aktualisieren
-
-    with open(filename, "w") as file:
-        json.dump( data, file, indent=4)
-
-
+# XP eines Users um bestimmten Wert erhöhen:
 def add_xp(user_id , xp):
-    def todo( userdata ):
-        userdata["xp"] += xp
-        return userdata
-    modify_user( user_id, todo)
+    with ModStatsData( user_id ) as data:
+        data.user["xp"] += xp
 
+# Spielstatistiken 
 def add_player_stats(user_id, gamename, won ):
-    def todo( userdata ):
-        if not gamename in userdata:
-            userdata[gamename] = [ 0,0 ]
-        userdata[gamename][0] += 1  # Anzahl der gesamten Spiele +1
-        if won:
-            userdata[gamename][1] += 1
-        return userdata
-    modify_user( user_id, todo)
+    with ModStatsData( user_id ) as data:
+        if not gamename in data.user:  # Falls für dieses Spiel noch keine Statistik existiert
+            data.user[gamename] = [ 0,0 ]   # neue Statistik anlegen
+        data.user[gamename][0] += 1  # Anzahl der gesamten Spiele +1
+        if won:                      # War das Spiel gewonnen?
+            data.user[gamename][1] += 1  # Dann Anzahl der gewonnenen Spiele erhöhen
+
