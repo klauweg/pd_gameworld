80e1a44931129661160963c1633c65839f555dcb
PlaceblockHD <felix.weglehner@gmx.de>
2020-08-14 17:23:11 +0200

    1. Addad async save for tictactoe (in tictactoegame.py there is no game.change_to_next_player() but in gamelogic line 125 there is write down this function

--- src/tictactoe/GameLogic.py
+++ src/tictactoe/GameLogic.py
@@ -20,16 +20,16 @@ class TicTacToeGameLogic(commands.Cog):
     async def tictactoe(self, ctx: commands.Context):
         if ctx.channel.id is not self.inviteChannel and ctx.author.bot is True:
             return
-        if self.queue.__contains__(ctx.author.id):
-             self.queue.remove(ctx.author.id)
-             embed = discord.Embed(title="See you soon!", description=f"""{ctx.author.display_name} left the Queue""",
-                                   color=0x49ff35)
-             embed.set_author(name="ConnectFour",
-                              icon_url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
-             embed.set_thumbnail(
-                 url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
-             await ctx.channel.send(embed=embed)
-             return
+        #if self.queue.__contains__(ctx.author.id):
+        #     self.queue.remove(ctx.author.id)
+        #     embed = discord.Embed(title="See you soon!", description=f"""{ctx.author.display_name} left the Queue""",
+        #                           color=0x49ff35)
+        #     embed.set_author(name="ConnectFour",
+        #                      icon_url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
+        #     embed.set_thumbnail(
+        #         url="https://cdn.discordapp.com/app-icons/742032003125346344/e4f214ec6871417509f6dbdb1d8bee4a.png?size=256")
+        #     await ctx.channel.send(embed=embed)
+        #     return
         guild: discord.Guild = ctx.guild
 
         if self.queue.__len__() >= 1:
@@ -82,48 +82,55 @@ class TicTacToeGameLogic(commands.Cog):
             return
         if self.channels_in_use.keys().__contains__(channel.id) and not self.channels_in_use.get(
                 channel.id).is_empty():
-            game = self.channels_in_use.get(channel.id)
+            game: TicTacToeGame.TicTacToeGame = self.channels_in_use.get(channel.id)
             # checks if player is player of this round
             if game.players.__contains__(message.author.id):
                 # checks if its player turn
                 if game.currentPlayer is message.author.id:
-                    id = game.fields.get(message.content)
-                    # checks if selected field exists
-                    if id is not None:
-                        isPlaced = game.placedFields.get(id) != 99
-                        # checks if field is empty
-                        if not isPlaced:
-                            # set field placed
-                            game.placedFields[id] = game.players.index(game.currentPlayer) + 1
-                            await channel.purge()
-                            await channel.send(file=await self.build_board(game.placedFields))
-                            if game.compute_winner() == 1:
-                                embed = discord.Embed(title=":tada: Player " + self.bot.get_user(game.players[1]).name +
-                                                            " won :tada:",
-                                                      colour=discord.Colour.green())
-                                await channel.send(embed=embed)
-                                await self.stopGame(channel.id)
-                                return
-                            elif game.compute_winner() == 0:
-                                embed = discord.Embed(
-                                    title=":tada: Player " + self.bot.get_user(game.players[0]).name + " won :tada:",
-                                    colour=discord.Colour.green())
-                                await channel.send(embed=embed)
-                                await self.stopGame(channel.id)
-                                return
-                            elif game.compute_winner() == -2:
-                                embed = discord.Embed(title=":crossed_swords:Undecided:crossed_swords: ",
-                                                      color=0xffe605)
-                                await channel.send(embed=embed)
-                                await self.stopGame(channel.id)
-                                return
-                            game.change_to_next_player()
-                            self.channels_in_use[channel.id] = game
-                    else:
-                        embed = discord.Embed(title=":loudspeaker: The Field is not valid :loudspeaker:",
-                                              colour=discord.Colour.red())
-                        await channel.send(embed=embed, delete_after=10)
-                        await message.delete(delay=10)
+                    if game.is_working == False:
+                        game.is_working = True
+                        id = game.fields.get(message.content)
+                        # checks if selected field exists
+                        if id is not None:
+                            isPlaced = game.placedFields.get(id) != 99
+                            # checks if field is empty
+                            if not isPlaced:
+                                # set field placed
+                                game.placedFields[id] = game.players.index(game.currentPlayer) + 1
+                                await channel.purge()
+                                await channel.send(file=await self.build_board(game.placedFields))
+                                if game.compute_winner() == 1:
+                                    embed = discord.Embed(title=":tada: Player " + self.bot.get_user(game.players[1]).name +
+                                                                " won :tada:",
+                                                          colour=discord.Colour.green())
+                                    await channel.send(embed=embed)
+                                    await self.stopGame(channel.id)
+                                    game.is_working = False
+                                    return
+                                elif game.compute_winner() == 0:
+                                    embed = discord.Embed(
+                                        title=":tada: Player " + self.bot.get_user(game.players[0]).name + " won :tada:",
+                                        colour=discord.Colour.green())
+                                    await channel.send(embed=embed)
+                                    await self.stopGame(channel.id)
+                                    game.is_working = False
+                                    return
+                                elif game.compute_winner() == -2:
+                                    embed = discord.Embed(title=":crossed_swords:Undecided:crossed_swords: ",
+                                                          color=0xffe605)
+                                    await channel.send(embed=embed)
+                                    await self.stopGame(channel.id)
+                                    game.is_working = False
+                                    return
+                                game.change_to_next_player()
+                                self.channels_in_use[channel.id] = game
+                            game.is_working = False
+                        else:
+                            embed = discord.Embed(title=":loudspeaker: The Field is not valid :loudspeaker:",
+                                                  colour=discord.Colour.red())
+                            await channel.send(embed=embed, delete_after=10)
+                            await message.delete(delay=10)
+                            game.is_working = False
 
                 else:
                     embed = discord.Embed(title=":loudspeaker: It is not your turn :loudspeaker:",
