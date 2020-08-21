f271a1317b015d01327a4d94ace55029aa7da886
Klaus W <vortex25@gmx.de>
2020-08-21 09:08:20 +0200

    Queue hat jetzt __queued_members __playing_members, !leave ist möglich, Spiele geben die Spieler wieder frei.

--- src/GameAPI/Queue.py
+++ src/GameAPI/Queue.py
@@ -1,25 +1,69 @@
-class Queue:
-    __occupied_players=[]
+import logging
 
-    def __init__(self):
+class Queue:
+    __queued_members={}   # member.id -> queuename
+    __playing_members={}  # member.id -> gamename (=queuename)
+    
+    def __init__(self, name):
+        self.queuename = name
         self.queue = []
-        self.add_action = None # Callbackfunktion für die Spiele
+        self.on_queue_change = None # Callbackfunktion für die Gamecontroller
+
+    # Wenn sich der Inhalt der Queue verändert:
+    def do_on_queue_change( self ):
+        logger.info( self.queuename + "queue changed:" )
+        logger.info( "  queued members: " + str(Queue.__queued_members) )
+        logger.info( "  playing members: " + str(Queue.__playing_members) )
+        logger.info( "  queue content: " + str(self.queue) )
+        # Die vom Gamecontroller registrierte Callbackfunktion ausführen
+        self.on_queue_change()
+    
+########## Queue INPUT
 
     # Neue Spieler in die Queue
     async def append(self, ctx):
-        if False: #ctx.author in Queue.__occupied_players:
-            await ctx.channel.send( str( ctx.author ) + " ist schon in einer Queue", delete_after=10)
+        if ctx.author.id in Queue.__queued_members:
+            await ctx.channel.send( str( ctx.author ) + 
+                                    " ist schon in der queue " + 
+                                    str(Queue.__queued_members[ ctx.author.id ]), delete_after=10)
+        elif ctx.author.id in Queue.__playing_members:
+            await ctx.channel.send( str( ctx.author ) + 
+                                    " spielt schon " + 
+                                    str(Queue.__playing_members[ ctx.author.id ]), delete_after=10)
         else:
-            Queue.__occupied_players.append( ctx.author)
+            Queue.__queued_members[ ctx.author.id ] = self.queuename
             self.queue.append(ctx)
-            await self.add_action()  # Callbackfunktion ausführen
+            self.do_on_queue_change()
 
-    def release_player(self, player):
-        self.__occupied_players.remove( player )
-
-    # Hier nehmen die Spiele ihre Player aus der Queue:
-    def pop(self):
-        return self.queue.pop(0)
+    # Spieler verlässt die Queue selbst:
+    async def remove(self, ctx):
+        if ctx.author.id in Queue.__queued_members:
+            Queue.__queued_members.pop( ctx.author.id )
+            self.queue = [ x  for x in self.queue  if x.author.id != ctx.author.id ]
+            await ctx.channel.send( str( ctx.author ) + 
+                                    " verlässt die Queue " + self.name, delete_after=10 )
+            self.do_on_queue_change()
+            
+                                    
+############ Queue OUTPUT
 
     def len(self):
         return len(self.queue)
+    
+    # Wenn ein Spiel beendet ist, muss diese funktion aufgerufen werden:
+    # Das kann letztlich das Spiel nur selbst machen, da es möglich ist, dass
+    # Spieler aus einem Spiel ausscheiden, bevor das Spiel komplett beendet ist.
+    def release_player(self, playerid):
+        Queue.__playing_members.pop( playerid )
+        
+    # Hier holen die Spiele ihre Player (contexte) aus der Queue:
+    # Der Member kommt aus der queued liste in die playing liste
+    def pop(self):
+        ctx = self.queue.pop(0)
+        Queue.__queued_members.pop( ctx.author.id )
+        Queue.__playing_members[ ctx.author.id ] = self.queuename
+        self.do_on_queue_change()
+        return ctx
+    
+
+
--- src/pd_gameworld.py
+++ src/pd_gameworld.py
@@ -3,41 +3,55 @@ from discord.ext import commands
 from discord.ext.commands import CommandNotFound, MissingRequiredArgument
 
 from parse import parse
+import logging
 from GameAPI.Queue import Queue
 from StatsCmd.StatsCommandFile import StatsCommand
 from bugreport.BugReport import BugReport
 
 #from tictactoe.GameLogic import TicTacToeGameLogic
-from connectfour.Gamelogic import GameControl as connectfour_GameControl
+import connectfour.Gamelogic
 #from hangman.GameLogic import HangManGameLogic
+
+logging.basicConfig(level=logging.INFO)
 
 client = commands.Bot(command_prefix="!")
 
-# join-channelid -> Spieleklasse
+client.add_cog(StatsCommand())
+
+# join-channelid -> Spieleklasse, Queuename, (Queue)
 games = {
-#    743463967996903496: [HangManGameLogic],
-#    741835475085557860: [TicTacToeGameLogic],
-    743425069216170024: [connectfour_GameControl]
+#    743463967996903496: [HangManGameLogic, "HangMan" ],
+#    741835475085557860: [TicTacToeGameLogic, "TicTacToe" ],
+    743425069216170024: [connectfour.Gamelogic.GameControl, "ConnectFour" ]
 }
 
 
 # Erzeugen der GameQueues und Spielekontrollobjekt koppeln:
 for channelid in games:
-    gamequeue = Queue()  # Queue erzeugen
+    gamequeue = Queue( games.[channelid][1] )  # Queue erzeugen und Namen zuweisen
     games[channelid].append(gamequeue) # Die Queue dem Join Channel zuordnen
-    games[channelid][0](gamequeue) # Spiel mit Kopplung an die Queue erzeugen
+    games[channelid][0]( gamequeue ) # Gamecontroller des Spiels erzeugen, queue übergeben
 
-
+    
 # Jemand will einem Spiel joinen und landet in der Queue:
 @client.command()
 async def join(ctx: commands.Context):
     await ctx.message.delete()
     if ctx.channel.id in games:
-        # ctx object in der queue speichern:
-        queue = games[ctx.channel.id][1] #zugehörige queue zum Channel ermitteln
+        queue = games[ctx.channel.id][1]
         await queue.append(ctx)
 
+        
+# Jemand will eine Queue verlassen:
+@client.command()
+async def leave(ctx: commands.Context):
+    await ctx.message.delete()
+    if ctx.channel.id in games:
+        queue = games[ctx.channel.id][1]
+        await queue.remove(ctx)
 
+
+# Der Chef darf Channels purgen:
 @client.command()
 async def purge(ctx: commands.Context, *, member: discord.Member = None):
     role = discord.utils.get(ctx.guild.roles, id=744630374855868456)
@@ -45,12 +59,14 @@ async def purge(ctx: commands.Context, *, member: discord.Member = None):
         await ctx.channel.purge()
 
 
+# Neue User im Welcome Channel begrüßen:
 @client.event
 async def on_member_join(member: discord.Member):
     channel = client.get_channel(741965363549569034)
     await channel.send(f"""Welcome to the Server {member.mention} !""")
 
-
+    
+# Nicht existierende oder fehlerhafte Commands werden aus dem Channel gelöscht:
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound):
@@ -62,19 +78,22 @@ async def on_command_error(ctx, error):
     raise error
 
 
+# Aufräumen beim Start:
 @client.event
 async def on_ready():
     # Alte Gamechannels löschen:
     for channel in client.get_all_channels():
         if parse("{}-{:d}", channel.name):
+            logging.info( "delete "+channel.name )
             await channel.delete()
+    # Inhalt der Join Channels löschen:
     for channelid in games:
-        await client.get_channel(channelid).purge()
+        channel = client.get_channel(channelid)
+        logging.info( "cleanup "+channel.name )
+        await channel.purge()
 
-client.add_cog(StatsCommand())
-
-token_file = open("../resources/privates.txt")
-client.run(token_file.readline())
-
-
+        
+with open("../resources/privates.txt") as token_file:
+    token = token_file.readline()
+client.run(token)
 
