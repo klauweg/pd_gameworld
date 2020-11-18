5397d6db30efbb4069ab575616e09485933367a8
Klaus W <klauweg@gmx.de>
2020-11-18 23:12:13 +0100

    Gametask in tictactoe jetzt mit Queue

--- src/Games/tictactoe.py
+++ src/Games/tictactoe.py
@@ -17,105 +17,102 @@ fieldmap={ "A1":0, "B1":1, "C1":2,
            "A3":6, "B3":7, "C3":8 }
 
 # Spielklasse
-# Der erste Spieler in den <players> ist üblicherweise der owner
-# Dieser Spieler wird immer ausgewählt. Falls mehr als ein weiterer
-# Spieler übergeben wird, so wird aus diesen der zweite Spieler
-# zufällig gewählt.
+# Parameter:
+# <channel> Kanal in dem das Spiel stattfindet
+# <players> Liste der Spieler, index 0 ist der Initiator des Spiels
 class Game():
     def __del__(self): # for debug only
         logger.info("Game Object has been destroyed.")
         
     def __init__(self, channel, players):
         self.gamechannel = channel
-        # 1. und zufälligen weiteren Player auswählen:
-        self.players = players[0], random.choice( players[1:] )
-        # Wer fängt an?
-        self.currentPlayer = random.choice( (0,1) )
-        # Das eigentliche Spielfeld:
-        self.playfield = 9*[None]
+        self.players = players
         self.playfield_message = None
-        
-        self.trigger = asyncio.Event()
-        self.lock = asyncio.Lock()
-        client.add_listener(self.on_message)
+        self.queue = asyncio.Queue()
         self.game_task = asyncio.create_task( self.game() )
+        client.add_listener(self.on_message)
         logger.info("Game has been started.")
 
-    # Beenden des Spiels, Aufräumen kann auch von Extern für Abbruch aufgerufen werden:
-    def endgame( self ):
+    # Beenden des Spiels:
+    async def destroygame( self ):
         logger.info("Spiel wird beendet.")
         for player in self.players:
             add_to_stats(player, "TicTacToe", 0, 1, 0)
             add_xp(player, 5)
-        self.game_task.cancel()
         client.remove_listener(self.on_message)
+        self.game_task.cancel()
         
-        
-    # MSG dispatcher (hier ist auch die eigentliche Spiellogik)
+    # MSG receiver:
     async def on_message(self, message):
         if message.channel.id != self.gamechannel.id:
             return # Die Nachricht war nicht aus unserem Channel
         if message.author.id == client.user.id:
             return # Nachrichten vom BOT werden ignoriert
-
-        # Solange das Spiel läuft, werden alle Nachrichten im Channel
-        # wieder gelöscht. Da der on_message() handler aber auch ausgelöst
-        # wird, wenn Befehle an das Party-Objekt gesendet werden, dieses seine
-        # Messages aber schon selbst löscht, könnte das hier fehlschlagen:
-        try:
-            await message.delete() # Nachricht im discord Channel löschen
-        except discord.errors.NotFound:
-            pass
+        # Nachrichten in die Queue zum Game Task schreiben:
+        self.queue.put( message )
         
-        async with self.lock: # Immer nur eine Msg gleichzeitig
-            self.trigger.set() # prevent timeout
-            # Leave Befehl:
-            if message.content.lower() == "leave" and message.author in self.players:
-                embed = discord.Embed(title="Das Spiel wird beendet, weil " +
-                         message.author.name + " keine Lust mehr hat.",
-                         colour=discord.Colour.green())
-                await self.gamechannel.send(embed=embed)
-                self.endgame()
-                return # Ein spieler hat das Spiel verlassen
+        
+    # Eigentlicher Game Task:
+    async def game( self ):
+        # Genug Spieler?
+        if self.players.len() < 2:
+            await self.msg_tooFewPlayers()
+            await self.destroygame()
+        # Spieler auswählen:
+        self.players = self.players[0], random.choice( self.players[1:] )
+        self.currentPlayer = random.choice( (0,1) )
+        # Spielfeld vorbereiten:
+        self.playfield = 9*[None]
+        # Spieler begrüßen und Spielfeld erstmalig ausgeben:
+        await self.msg_gameInstructions()
+        await self.msg_playfield()
+        
+        # Start Game, Wait for Messages with Timeout:
+        while True:
+            try:
+                message = await asyncio.wait_for(self.queue.get(), timeout=60*5)
+            except asyncio.TimeoutError:
+                logger.info("Timeout")
+                await self.msg_timeout()
+                await self.destroygame() #EXIT
 
-            # Welcher Spieler?
-            if message.author.id != self.players[self.currentPlayer].id:
-                return # Falscher Spieler
+            if message.content.lower() == "leave":
+                if message.author.id in [ player.id for player in self.players]:
+                    await message.delete()
+                    await self.msg_leave( message.author.name )
+                    self.players.remove( message.author )
+                    await self.destroygame() #EXIT
 
-            # Ist die Eingabe eine Spielfeldkoordinate?
             id = fieldmap.get( message.content.upper() )
-            if id == None: # Obacht: "0" ist auch "False"
-                return # Der String ist keine Spielfeldkoordinate
+            if id != None:
+                await message.delete()
+                # Richtiger Spieler?
+                if message.author.id != self.players[self.currentPlayer].id:
+                    await self.msg_wrongPlayer()
+                    continue
+                # Ist das Feld noch frei?
+                if self.playfield[ id ] != None:
+                    await self.msg_occupied()
+                    continue
 
-            if self.playfield[ id ] != None: # Das Feld ist schon benutzt
-                embed = discord.Embed(title=":loudspeaker: Kein erlaubtes Feld :loudspeaker:",
-                                      colour=discord.Colour.red())
-                await self.gamechannel.send(embed=embed, delete_after=2)
-                return # Das Feld war schon besetzt
+                self.playfield[id] = self.currentPlayer # Markierung setzen
+                await self.msg_playfield()
 
-            self.playfield[id] = self.currentPlayer # Markierung setzen
-            await self.send_playfield()
-
-            result = self.compute_winner()
-            if result == True: # Einer hat gewonnen
-                embed = discord.Embed(title=":tada: Player " + 
-                                self.players[self.currentPlayer].name +
-                                " hat gewonnen :tada:",
-                                colour=discord.Colour.green())
-                await self.gamechannel.send(embed=embed)
-                add_xp(self.players[self.currentPlayer], 20)
-                add_to_stats(self.players[self.currentPlayer], "TicTacToe", 1, 0, 0)
-                deposit_money(self.players[self.currentPlayer], 10)
-                self.endgame()
-            elif result == "pat": # Unentschieden
-                embed = discord.Embed(title=":tada: Unentschieden :tada:",
-                        colour=discord.Colour.green())
-                await self.gamechannel.send(embed=embed)
-                for player in self.players:
-                    add_to_stats(player, "TicTacToe", 0, 0, 1)
-                self.endgame()
-            else: # Geht noch weiter, Spieler wechseln
-                self.currentPlayer = 0 if self.currentPlayer == 1 else 1
+                result = self.compute_winner()
+                
+                if result == True: # Einer hat gewonnen
+                    await msg_won( self.players[self.currentPlayer].name )
+                    add_xp(self.players[self.currentPlayer], 20)
+                    add_to_stats(self.players[self.currentPlayer], "TicTacToe", 1, 0, 0)
+                    deposit_money(self.players[self.currentPlayer], 10)
+                    self.destroygame()
+                elif result == "pat": # Unentschieden
+                    await msg_pat()
+                    for player in self.players:
+                        add_to_stats(player, "TicTacToe", 0, 0, 1)
+                    self.destroygame()
+                else: # Geht noch weiter, Spieler wechseln
+                    self.currentPlayer = 0 if self.currentPlayer == 1 else 1
 
 
     # Durchsucht das Spielfeld nach drei gleichen Marken in einer Reihe
@@ -130,60 +127,86 @@ class Game():
 
         if not None in self.playfield: # Unentschieden
             return "pat"
-
-
-    # Spielfeld neu erzeugen:
-    async def send_playfield(self):
-        if self.playfield_message:
-            await self.playfield_message.delete()
-        self.playfield_message = await self.gamechannel.send(file=self.build_board())
         
-    def build_board(self):
-        field_img: Image.Image = Image.open("../resources/tictactoe/gamefield_universe.png")
-        o = Image.open("../resources/tictactoe/o_universe.png")
-        X = Image.open("../resources/tictactoe/x_universe.png")
-        fields = [(12, 12), (175, 12), (337, 12),
+########################################################################################
+    # Zu sendende Nachrichten:
+    async def msg_pat(self):
+        embed = discord.Embed(
+           title=":tada: Unentschieden :tada:",
+           colour=discord.Colour.green())
+        await self.gamechannel.send(embed=embed)
+        
+    async def msg_won(self, name):
+        embed = discord.Embed(
+           title=":tada: Player " + name + " hat gewonnen :tada:",
+           colour=discord.Colour.green())
+        await self.gamechannel.send(embed=embed)
+
+    async def msg_wrongPlayer(self):
+        embed = discord.Embed(
+            title=":loudspeaker: Du bist nicht dran :loudspeaker:",
+            colour=discord.Colour.red())
+        await self.gamechannel.send(embed=embed, delete_after=2)
+        
+    async def msg_occupied(self):
+        embed = discord.Embed(
+            title=":loudspeaker: Kein erlaubtes Feld :loudspeaker:",
+            colour=discord.Colour.red())
+        await self.gamechannel.send(embed=embed, delete_after=2)
+            
+    async def msg_leave(self, name):
+        embed = discord.Embed(
+            title="Das Spiel wird beendet, weil " +
+                    name + " keine Lust mehr hat.",
+            colour=discord.Colour.green())
+        await self.gamechannel.send(embed=embed, delete_after=7)
+        
+    async def msg_timeout(self):
+        embed = discord.Embed(title="Game Gestoppt:",
+           description="(Timeout)", color=0x58ff46)
+           embed.set_author(name="TicTacToe")
+        await self.gamechannel.send(embed=embed, delete_after=7)
+
+    async def msg_tooFewPlayers(self):
+        embed = MyEmbed(name="TicTacToe Game",
+           title="Fehler",
+           description="Das Spiel braucht mindestens zwei Spieler"
+             "oder mehr aus denen dann zufällig gewählt wird.",
+           color=0xFF0000)
+        await self.gamechannel.send(embed=embed, delete_after=10)            
+    
+    async def msg_gameInstructions(self):
+        current_player = self.players[self.currentPlayer].display_name
+        embed = MyEmbed( name="TicTacToe",
+                         title="Also, "+current_player+" bitte beginne!",
+                         description="", color=0x58ff46)
+        embed.add_field(name="Wie wähle ich ein Feld aus?",
+                        value="A1   B1   C1\n"
+                             +"A2  B2  C2\n"
+                             +"A3  B3  C3",
+                        inline=True)
+        embed.add_field(name="Und jetzt?",
+                        value="Schreibe das gewählte Feld in den Channel!",
+                        inline=True)
+        await self.gamechannel.send(embed=embed, delete_after=30)
+
+    async def msg_playfield(self):
+        field_img = Image.open("../resources/tictactoe/gamefield_universe.png")
+        marker_imgs = ( Image.open("../resources/tictactoe/o_universe.png"),
+                        Image.open("../resources/tictactoe/x_universe.png") )
+        coords = [(12, 12), (175, 12), (337, 12),
                   (12, 175), (175, 175), (337, 175),
                   (12, 337), (175, 337), (337, 337)]
         for fieldno in range(0,9):
-            if self.playfield[fieldno] != None:
-                symbol = X if self.playfield[fieldno]==0 else o
-                field_img.paste( symbol, fields[fieldno], symbol )
+            if self.playfield[fieldno] is not None: # Feld besetzt?
+                symbol = marker_imgs[ self.playfield[ fieldno ] ]
+                field_img.paste( symbol, coords[fieldno], symbol )
         arr = io.BytesIO()
         field_img.save(arr, format="png")
         arr.seek(0)
         file = discord.File(arr)
         file.filename = "field.png"
-        return file
-
-    
-    # Async Game Task, für Zeitsteuerung u.ä.:
-    async def game( self ):
-        logger.info("Timer started")
-        # spiel initialisieren:
-        async with self.lock:
-            current_player = self.players[self.currentPlayer].display_name
-            embed = MyEmbed( name="TicTacToe",
-                             title="Also, "+current_player+" bitte beginne!",
-                             description="", color=0x58ff46)
-            embed.add_field(name="Wie wähle ich ein Feld aus?",
-                            value="A1   B1   C1\n"
-                                 +"A2  B2  C2\n"
-                                 +"A3  B3  C3",
-                            inline=True)
-            embed.add_field(name="Und jetzt?",
-                            value="Schreibe das gewählte Feld in den Channel!",
-                            inline=True)
-            await self.gamechannel.send(embed=embed, delete_after=30)
-            await self.send_playfield()
-        # Wait for Timeout:
-        while True:
-            try:
-                await asyncio.wait_for(self.trigger.wait(), timeout=60*5)
-            except asyncio.TimeoutError:
-                logger.info("Timeout")
-                embed = discord.Embed(title="Game Gestoppt:", description="(Timeout)", color=0x58ff46)
-                embed.set_author(name="TicTacToe")
-                await self.gamechannel.send(embed=embed)
-                self.endgame()
-
+        if self.playfield_message:
+            await self.playfield_message.delete()
+        self.playfield_message = await self.gamechannel.send(file=file)
+        
